2c32345e945a966e2adfb419edd661ab3951f305
Leoryu90 <thetein@gmail.com>
2024-07-23 03:20:34 +0900

    :memo: 20240722 KYH finish

---
+++ hello-demo/src/main/java/hello/hello_demo/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hello_demo.repository;
+
+import hello.hello_demo.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
