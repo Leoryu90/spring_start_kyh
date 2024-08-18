be37ca02dd064fa4676b611523d55ffdb71c0d33
Seokho ryu <thetein@gmail.com>
2024-08-18 18:08:09 +0900

    20240818 kyh JDBC study

--- hello-demo/src/main/java/hello/hello_demo/SpringConfig.java
+++ hello-demo/src/main/java/hello/hello_demo/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hello_demo;
 
+import hello.hello_demo.repository.JdbcMemberRepository;
 import hello.hello_demo.repository.MemberRepository;
 import hello.hello_demo.repository.MemoryMemberRepository;
 import hello.hello_demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
